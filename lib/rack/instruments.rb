9f9b2a8e078673f1aee6cf7468d56523c7d8f726
Brandur <brandur@mutelight.org>
2012-06-10 23:21:40 +0200

    Add request IDs for tracing

--- lib/rack/instruments.rb
+++ lib/rack/instruments.rb
@@ -1,3 +1,5 @@
+require "uuidtools"
+
 module Rack
   class Instruments
     def initialize(app)
@@ -5,13 +7,24 @@ module Rack
     end
 
     def call(env)
-      @start_request = Time.now
-      status, headers, response = @app.call(env)
-      Scrolls.log :instrumentation,
+      request_id = UUIDTools::UUID.timestamp_create.to_s
+      request_start = Time.now
+      Scrolls.log :instrumentation_start,
+        id: request_id,
         method: env["REQUEST_METHOD"],
         route: env["REQUEST_PATH"],
-        elapsed: "#{Integer((Time.now - @start_request) * 1000)}ms",
         ip: env["REMOTE_ADDR"]
+
+      env["REQUEST_ID"] = request_id
+      status, headers, response = @app.call(env)
+
+      Scrolls.log :instrumentation_end,
+        id: request_id,
+        method: env["REQUEST_METHOD"],
+        route: env["REQUEST_PATH"],
+        ip: env["REMOTE_ADDR"],
+        elapsed: "#{Integer((Time.now - request_start) * 1000)}ms"
+
       [status, headers, response]
     end
   end
