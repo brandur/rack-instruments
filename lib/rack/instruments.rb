d0c116fcfe14f09f22451366f4874601bcb9de78
Brandur <brandur@mutelight.org>
2012-06-10 23:44:34 +0200

    start/finish + id last + aligning

--- lib/rack/instruments.rb
+++ lib/rack/instruments.rb
@@ -9,22 +9,24 @@ module Rack
     def call(env)
       request_id = UUIDTools::UUID.timestamp_create.to_s
       request_start = Time.now
-      Scrolls.log :instrumentation_start,
-        id: request_id,
+      Scrolls.log :instrumentation,
+        at:     start,
         method: env["REQUEST_METHOD"],
-        route: env["REQUEST_PATH"],
-        ip: env["REMOTE_ADDR"]
+        route:  env["REQUEST_PATH"],
+        ip:     env["REMOTE_ADDR"],
+        id:     request_id
 
       env["REQUEST_ID"] = request_id
       status, headers, response = @app.call(env)
 
-      Scrolls.log :instrumentation_end,
-        id: request_id,
-        method: env["REQUEST_METHOD"],
-        route: env["REQUEST_PATH"],
-        ip: env["REMOTE_ADDR"],
-        status: status,
-        elapsed: "#{Integer((Time.now - request_start) * 1000)}ms"
+      Scrolls.log :instrumentation,
+        at:      finish,
+        method:  env["REQUEST_METHOD"],
+        route:   env["REQUEST_PATH"],
+        ip:      env["REMOTE_ADDR"],
+        status:  status,
+        elapsed: "#{Integer((Time.now - request_start) * 1000)}ms",
+        id:      request_id
 
       [status, headers, response]
     end
