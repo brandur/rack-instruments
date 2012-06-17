9bb41f529206255fa25e9395a0f4784f98bc9462
Brandur <brandur@mutelight.org>
2012-06-17 12:22:18 -0700

    Cleaner implementation + tests

--- lib/rack/instruments.rb
+++ lib/rack/instruments.rb
@@ -9,25 +9,20 @@ module Rack
     def call(env)
       request_id = UUIDTools::UUID.timestamp_create.to_s
       request_start = Time.now
-      Scrolls.log :instrumentation,
-        at:     "start",
-        method: env["REQUEST_METHOD"],
-        route:  env["REQUEST_PATH"],
-        ip:     env["REMOTE_ADDR"],
-        id:     request_id
+      status, headers, response = nil, nil, nil
 
+      # make ID of the request accessible to consumers down the stack
       env["REQUEST_ID"] = request_id
-      status, headers, response = @app.call(env)
 
-      Scrolls.log :instrumentation,
-        at:      "finish",
-        method:  env["REQUEST_METHOD"],
-        route:   env["REQUEST_PATH"],
-        ip:      request.env["X-FORWARDED-FOR"] ||
-          request.env["HTTP_X_FORWARDED_FOR"] || env["REMOTE_ADDR"],
-        status:  status,
-        elapsed: "#{Integer((Time.now - request_start) * 1000)}ms",
-        id:      request_id
+      Scrolls.log(:instrumentation,
+        method: env["REQUEST_METHOD"],
+        path:   env["REQUEST_PATH"],
+        ip:     env["X-FORWARDED-FOR"] || env["HTTP_X_FORWARDED_FOR"] ||
+          env["REMOTE_ADDR"],
+        id:     request_id,
+        status: -> { status }) do
+          status, headers, response = @app.call(env)
+      end
 
       [status, headers, response]
     end
---
+++ test/instruments_test.rb
@@ -0,0 +1,64 @@
+require "stringio"
+require "test_helper"
+
+module Scrolls
+  def self.log(event, attrs)
+    $attrs = attrs
+    yield if block_given?
+  end
+end
+
+describe Rack::Instruments do
+  before do
+    $attrs, $env = nil, nil
+  end
+
+  def app
+    ->(env) { $env = env ; [200, {}, ""] }
+  end
+
+  def call(env = {})
+    Rack::Instruments.new(app).call(env)
+  end
+
+  it "reads request method" do
+    call("REQUEST_METHOD" => "GET")
+    $attrs[:method].must_equal "GET"
+  end
+
+  it "reads request path" do
+    call("REQUEST_PATH" => "/index")
+    $attrs[:path].must_equal "/index"
+  end
+
+  it "prefers to read IP from X-FORWARDED-FOR" do
+    call("X-FORWARDED-FOR" => "1.2.3.4", "REMOTE_ADDR" => "2.3.4.5")
+    $attrs[:ip].must_equal "1.2.3.4"
+  end
+
+  it "prefers to read IP from HTTP_X_FORWARDED_FOR" do
+    call("HTTP_X_FORWARDED_FOR" => "1.2.3.4", "REMOTE_ADDR" => "2.3.4.5")
+    $attrs[:ip].must_equal "1.2.3.4"
+  end
+
+  it "will read IP from REMOTE_ADDR without a proxy" do
+    call("REMOTE_ADDR" => "2.3.4.5")
+    $attrs[:ip].must_equal "2.3.4.5"
+  end
+
+  it "includes a request ID" do
+    call()
+    $attrs[:id].must_match /^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$/
+  end
+
+  it "injects a request ID into the environment" do
+    call()
+    $env["REQUEST_ID"].must_equal $attrs[:id]
+  end
+
+  it "includes the status that bubbled up" do
+    call()
+    # normally, the logger will call this lambda
+    $attrs[:status].call.must_equal 200
+  end
+end
